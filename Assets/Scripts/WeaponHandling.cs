975962b21a0db5418a881aaef8a82bf39a3fa1e9
Conrad Wilson <conradjwilson@hotmail.com>
2021-06-15 11:57:04 +0100

    Week 11 Upload

--- Assets/Scripts/WeaponHandling.cs
+++ Assets/Scripts/WeaponHandling.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class WeaponHandling : MonoBehaviour
 {
     public Transform weapon;
+    public XRController dominantHand;
     [SerializeField] private XRController leftController;
     [SerializeField] private XRController rightController;
     [SerializeField] private Transform attachTransform1;
     [SerializeField] private Transform attachTransform2;
 
+    private XRController nonDominantHand;
+    private XRController lastSingleGrippingHand;
+    private XRController lastSingleFreeHand;
     private bool canEnterSingleGrip;
+    private Vector3 lastSingleGrippingHandAttachTransformLocalPos;
+    private Quaternion lastSingleGrippingHandAttachTransformLocalRot;
+
+    private void Start()
+    {
+        nonDominantHand = rightController == dominantHand ? leftController : rightController;
+
+        lastSingleGrippingHand = dominantHand;
+        lastSingleFreeHand = nonDominantHand;
+
+        lastSingleGrippingHandAttachTransformLocalPos = lastSingleGrippingHand.attachTransform.localPosition;
+        lastSingleGrippingHandAttachTransformLocalRot = lastSingleGrippingHand.attachTransform.localRotation;
+    }
 
     void Update()
     {
@@ -26,41 +43,75 @@ public class WeaponHandling : MonoBehaviour
     {
         if (rightController.isGripActivated && leftController.isGripActivated)
         {
+            if (!canEnterSingleGrip)
+                canEnterSingleGrip = true;
+
+            // ### Do two-handed code here ###
+
+            Vector3 handGripDifference = lastSingleFreeHand.attachTransform.position - lastSingleGrippingHand.attachTransform.position;
+            Quaternion newRot = Quaternion.identity;
+
+            if (weapon.InverseTransformDirection(handGripDifference).z >= 0)
+            {
+                // Second hand that is now gripping on is ^ above ^ first gripping hand along the shaft
+                newRot = Quaternion.LookRotation(handGripDifference);
+            }
+            else
+            {
+                // Second hand that is now gripping on is v below v first gripping hand along the shaft
+                Vector3 inverseHandGripDifference = lastSingleGrippingHand.attachTransform.position - lastSingleFreeHand.attachTransform.position;
+                newRot = Quaternion.LookRotation(inverseHandGripDifference);
+            }
+
+            //weapon.localPosition = newPos;
+            lastSingleGrippingHand.attachTransform.rotation = newRot;
+
             Debug.Log("Both controllers are gripping right now");
-            canEnterSingleGrip = true;
         }
         else if (rightController.isGripActivated && canEnterSingleGrip)
         {
-            weapon.SetParent(rightController.attachTransform);
-            weapon.localPosition = -attachTransform1.localPosition;
-            weapon.localRotation = attachTransform1.localRotation;
-            canEnterSingleGrip = false;
+            if (weapon.parent != rightController.attachTransform)
+            {
+                weapon.SetParent(rightController.attachTransform);
+                ResetLastSingleGrippingHand();
+                weapon.localPosition = -attachTransform1.localPosition;
+                weapon.localRotation = attachTransform1.localRotation;
+                lastSingleGrippingHand = rightController;
+                lastSingleFreeHand = leftController;
+                canEnterSingleGrip = false;
+            }
         }
         else if (leftController.isGripActivated && canEnterSingleGrip)
         {
-            weapon.SetParent(leftController.attachTransform);
-            weapon.localPosition = -attachTransform1.localPosition;
-            weapon.localRotation = attachTransform1.localRotation;
-            canEnterSingleGrip = false;
+            if (weapon.parent != leftController.attachTransform)
+            {
+                weapon.SetParent(leftController.attachTransform);
+                ResetLastSingleGrippingHand();
+                weapon.localPosition = -attachTransform1.localPosition;
+                weapon.localRotation = attachTransform1.localRotation;
+                lastSingleGrippingHand = leftController;
+                lastSingleFreeHand = rightController;
+                canEnterSingleGrip = false;
+            }
         }
+    }
+
+    private void ResetLastSingleGrippingHand()
+    {
+        lastSingleGrippingHand.attachTransform.localPosition = lastSingleGrippingHandAttachTransformLocalPos;
+        lastSingleGrippingHand.attachTransform.localRotation = lastSingleGrippingHandAttachTransformLocalRot;
     }
 
     private void UpdateWeaponSpawnState()
     {
-        if (!weapon.gameObject.activeInHierarchy && (rightController.isGripActivated || leftController.isGripActivated))
+        if (!weapon.gameObject.activeInHierarchy && (dominantHand.isGripActivated))
         {
-            if (rightController.isGripActivated)
-            {
-                SpawnWeapon(rightController);
-            }
-            else
-            {
-                SpawnWeapon(leftController);
-            }
+            SpawnWeapon(dominantHand);
             canEnterSingleGrip = true;
         }
         else if (weapon.gameObject.activeInHierarchy && !rightController.isGripActivated && !leftController.isGripActivated)
         {
+            ResetLastSingleGrippingHand();
             weapon.SetParent(null);
             DespawnWeapon();
         }
@@ -69,6 +120,9 @@ public class WeaponHandling : MonoBehaviour
     private void SpawnWeapon(XRController controller)
     {
         weapon.gameObject.SetActive(true);
+        weapon.SetParent(dominantHand.attachTransform);
+        weapon.localPosition = -attachTransform1.localPosition;
+        weapon.localRotation = attachTransform1.localRotation;
     }
 
     private void DespawnWeapon()
