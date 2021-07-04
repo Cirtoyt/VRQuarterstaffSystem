4eccfacb51499e2dd8077572599adafc6351ece9
Conrad Wilson <conradjwilson@hotmail.com>
2021-07-04 19:28:11 +0100

    Got hands with physics!!

--- Assets/Scripts/XRPhysicsHand.cs
+++ Assets/Scripts/XRPhysicsHand.cs
@@ -7,6 +7,12 @@ public class XRPhysicsHand : MonoBehaviour
     [Header("Variables")]
     public XRController parentController;
     public Transform attachTransform;
+    [Range(0, 1)] [SerializeField] private float slowDownVelocity = 0.75f;
+    [Range(0, 1)] [SerializeField] private float slowDownAngularVelocity = 0.75f;
+    [Range(0, 100)] [SerializeField] private float maxPositionChange = 75;
+    [Range(0, 500)] [SerializeField] private float maxRotationChange = 250;
+    [SerializeField] private float positionSpeed = 20;
+    [SerializeField] private float rotationSpeed = 100;
     [Header("Statics")]
     public XRHandVisuals handVisuals;
 
@@ -17,23 +23,57 @@ public class XRPhysicsHand : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = 30f;
 
         enablePhysics = true;
+
+        transform.position = parentController.transform.position;
+        transform.rotation = parentController.transform.rotation;
     }
 
     private void Update()
     {
         if (enablePhysics)
         {
-            // Turn hand collider on? Should collider be off when disabled inputs?
-            // Have hand react to surroundings (colliders)
-
-            //rb.MovePosition(parentController.transform.position);
-            //rb.MoveRotation(parentController.transform.rotation);
-
             // Currently do not have hand physics and instant snap physics hand to controller position & rotation:
-            transform.position = parentController.transform.position;
-            transform.rotation = parentController.transform.rotation;
+            //rb.position = parentController.transform.position;
+            //rb.rotation = parentController.transform.rotation;
+
+            // Position
+            //rb.velocity *= slowDownVelocity;
+
+            //Vector3 posDifference = parentController.transform.position - rb.position;
+            //Vector3 newVelocity = posDifference / Time.deltaTime;
+
+            //if (!float.IsNaN(newVelocity.x) && !float.IsInfinity(newVelocity.x))
+            //{
+            //    float maxChange = maxPositionChange * Time.deltaTime;
+            //    rb.velocity = Vector3.MoveTowards(rb.velocity, newVelocity, maxChange);
+            //}
+
+            rb.velocity = (parentController.transform.position - transform.position) * positionSpeed;
+
+            // Rotation
+            //rb.angularVelocity *= slowDownAngularVelocity;
+
+            //Quaternion rotDifference = parentController.transform.rotation * Quaternion.Inverse(rb.rotation);
+            //rotDifference.ToAngleAxis(out float angleInDegrees, out Vector3 rotationAxis);
+
+            //if (angleInDegrees > 180)
+            //    angleInDegrees -= 360;
+
+            //Vector3 newAngularVelocity = (rotationAxis * angleInDegrees * Mathf.Deg2Rad) / Time.deltaTime;
+
+            //if (!float.IsNaN(newAngularVelocity.x) && !float.IsInfinity(newAngularVelocity.x))
+            //{
+            //    float maxChange = maxRotationChange * Time.deltaTime;
+            //    rb.angularVelocity = Vector3.MoveTowards(rb.angularVelocity, newAngularVelocity, maxChange);
+            //}
+
+            Quaternion rotDifference = parentController.transform.rotation * Quaternion.Inverse(transform.rotation);
+            rotDifference.ToAngleAxis(out float angleInDegrees, out Vector3 rotationAxis);
+
+            rb.angularVelocity = rotationAxis * angleInDegrees * Mathf.Deg2Rad * rotationSpeed;
         }
     }
 }
