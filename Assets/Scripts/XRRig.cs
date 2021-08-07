b6e0b8ecf9906008cb974906a2944bb8dc33b5ca
Conrad Wilson <conradjwilson@hotmail.com>
2021-08-07 12:31:28 +0100

    Fixed Quest build height issues

--- Assets/Scripts/XRRig.cs
+++ Assets/Scripts/XRRig.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class XRRig : MonoBehaviour
 {
     public Transform cameraTrans;
     public Vector3 cameraPosInRigSpace;
     public float cameraHeightInRigSpace;
-    
+    static XRInputSubsystem XRInputSubsystem;
+
+    private void Start()
+    {
+        List<XRInputSubsystem> subsystems = new List<XRInputSubsystem>();
+        SubsystemManager.GetInstances<XRInputSubsystem>(subsystems);
+        for (int i = 0; i < subsystems.Count; i++)
+        {
+            subsystems[i].TrySetTrackingOriginMode(TrackingOriginModeFlags.Floor);
+        }
+    }
+
     void Update()
     {
         cameraPosInRigSpace = transform.InverseTransformPoint(cameraTrans.transform.position);
